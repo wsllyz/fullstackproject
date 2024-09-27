52d103e25fc614dde15ed0d0a3772705ccdad132
Weslley Santana <westvsantana@gmail.com>
2024-09-27 01:00:10 -0300

    updateProject
    serializers and models delete

--- fullstackproject/home/models.py
+++ fullstackproject/home/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Pizza(models.Model):
-    nome = models.TextField(max_length=100)
-    ingredientes = models.TextField(max_length=100)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self) -> str:
-        return self.name
--- fullstackproject/home/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Pizza
-
-class PizzaSerializer(serializers.Modelserializer):
-    class Meta:
-        model = Pizza
-        fields = '__all__'
